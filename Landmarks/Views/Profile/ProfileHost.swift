964a0284ba093b145a38f177c917a68864fa23c1
Diego Molina <dimolinas@unal.edu.co>
2023-03-24 15:06:09 -0500

    Create a profile views and model

---
+++ Landmarks/Views/Profile/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by MacBookPro on 24/03/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
