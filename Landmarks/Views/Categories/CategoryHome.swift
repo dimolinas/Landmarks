82eb1e08195dc40a3fb46aed2d2c8486c1d6a4e1
Diego Molina <dimolinas@unal.edu.co>
2023-03-24 13:59:28 -0500

    we addded logo app, and created a new groups where i could used simple animation and draw a geometric figures

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by MacBookPro on 24/03/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
